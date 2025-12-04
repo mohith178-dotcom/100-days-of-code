e1b31f0cda7d5c00aca62b67151effb82c561669
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:10:58 +0530

    Add files via upload

---
+++ day46q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+
+    
+    while(str[i] != '\0') {
+        char ch = str[i];
+        
+        if(ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' &&
+           ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U') {
+            str[j] = str[i];
+            j++;
+        }
+        i++;
+    }
+    str[j] = '\0'; 
+
+    printf("String after removing vowels: %s\n", str);
+
+    return 0;
+}
