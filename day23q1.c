fe20da1a9ef781b3bcddde3903d9cfa91529d69d
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 22:41:45 +0530

    Add files via upload

---
+++ day23q1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        float numerator = 2 * i;
+        float denominator = 4 * i - 1;
+        sum += numerator / denominator;
+    }
+
+    printf("Sum of the series = %.4f", sum);
+
+    return 0;
+}
