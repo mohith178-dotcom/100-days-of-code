19731b5456d6f91e1bbfd8c8bcfbbc9c07592cd1
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:30:31 +0530

    Add files via upload

---
+++ day71q.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    int age;
+
+    
+    printf("Enter your name: ");
+    scanf("%[^\n]", name);  
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    
+    FILE *fp = fopen("info.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    fprintf(fp, "Name: %s\nAge: %d\n", name, age);
+
+
+    fclose(fp);
+
+    
+    printf("Data successfully saved to info.txt\n");
+
+    return 0;
+}
