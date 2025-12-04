7c19763a365fcd778e471c22a1c35aca4f992643
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:29:21 +0530

    Add files via upload

---
+++ day68q.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int sum = 0;
+
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    int total = n * (n + 1) / 2;   
+    int missing = total - sum;
+
+    printf("%d", missing);
+
+    return 0;
+}
