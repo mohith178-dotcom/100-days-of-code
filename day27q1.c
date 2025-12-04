ba548bf4e6bfbe30d2dc353c0779093bf11ac5a6
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 22:52:45 +0530

    Add files via upload

---
+++ day27q1.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+int main() {
+    int i, j, star;
+
+    
+    for(i = 1; i <= 5; i++) {
+        star = 2*i - 1;        
+        for(j = 1; j <= star; j++)
+            printf("*");
+        printf("\n");
+    }
+
+
+    for(i = 4; i >= 1; i--) {
+        star = 2*i - 1;        
+        for(j = 1; j <= star; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
+
