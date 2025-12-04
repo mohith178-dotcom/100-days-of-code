cac76e96478a2c23500cd3305cd603cff51ec5ca
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:09:10 +0530

    Add files via upload

---
+++ day44q1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+    int spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+
+    while(str[i] != '\0') {
+        char ch = str[i];
+
+        if(ch >= '0' && ch <= '9') {
+            digits++;
+        } else if(ch == ' ' || ch == '\n') {
+            spaces++;
+        } else if((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            
+        } else {
+            special++;
+        }
+
+        i++;
+    }
+
+    printf("Number of spaces = %d\n", spaces);
+    printf("Number of digits = %d\n", digits);
+    printf("Number of special characters = %d\n", special);
+
+    return 0;
+}
