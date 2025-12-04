08d350ab569eca093c9965888f43bd986f6b5ce9
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 22:58:19 +0530

    Add files via upload

---
+++ day33q2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, pos;
+
+    printf("Enter the number of elements in the sorted array: ");
+    scanf("%d", &n);
+
+    int arr[n + 1];  
+
+    
+    printf("Enter %d elements in sorted order:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to insert: ");
+    scanf("%d", &key);
+
+    
+    pos = n; 
