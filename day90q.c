a111eb39522f7a2d3cccf740087e0062d2f5c5d9
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:47:25 +0530

    Add files via upload

---
+++ day90q.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+};
+
+struct Person {
+    char name[20];
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+
+    // Sample data
+    printf("Enter name: ");
+    scanf("%s", p.name);
+
+    printf("Enter gender (0 = MALE, 1 = FEMALE, 2 = OTHER): ");
+    scanf("%d", (int*)&p.gender);
+
+    printf("\n--- Person Details ---\n");
+    printf("Name: %s\n", p.name);
+
+    switch (p.gender) {
+        case MALE:
+            printf("Gender: MALE\n");
+            break;
+        case FEMALE:
+            printf("Gender: FEMALE\n");
+            break;
+        case OTHER:
+            printf("Gender: OTHER\n");
+            break;
+        default:
+            printf("Gender: INVALID\n");
+    }
+
+    return 0;
+}
