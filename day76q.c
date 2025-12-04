218d187ca544b01559575d40adf1e4daad410e1d
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:32:30 +0530

    Add files via upload

---
+++ day76q.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char filename[100];
+    FILE *fp;
+    char ch;
+
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error: File does not exist or cannot be opened.\n");
+    } else {
+        printf("File content:\n");
+
+    
+        while ((ch = fgetc(fp)) != EOF) {
+            putchar(ch);
+        }
+
+        fclose(fp);
+    }
+
+    return 0;
+}
