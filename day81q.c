fd7c241f20b2056ae3cad2edcf0a41d658756630
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:35:23 +0530

    Add files via upload

---
+++ day81q.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+enum Days {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+};
+
+int main() {
+    enum Days d;
+
+    for (d = SUNDAY; d <= SATURDAY; d++) {
+        printf("%d ", d);
+        switch (d) {
+            case SUNDAY:    printf("SUNDAY\n"); break;
+            case MONDAY:    printf("MONDAY\n"); break;
+            case TUESDAY:   printf("TUESDAY\n"); break;
+            case WEDNESDAY: printf("WEDNESDAY\n"); break;
+            case THURSDAY:  printf("THURSDAY\n"); break;
+            case FRIDAY:    printf("FRIDAY\n"); break;
+            case SATURDAY:  printf("SATURDAY\n"); break;
+        }
+    }
+
+    return 0;
+}
