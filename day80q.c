4abff0cb5c855d14fbdf46ce50b6dce3cba8fb87
mohith178-dotcom <mohithratna1708@gmail.com>
2025-12-04 23:33:45 +0530

    Add files via upload

---
+++ day80q.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+    FILE *fp;
+    int n, i;
+
+    
+    fp = fopen("students.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file for writing.\n");
+        return 1;
+    }
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("\nEnter details of student %d\n", i);
+        
+        printf("Name: ");
+        scanf("%s", s.name);
+
+        printf("Roll Number: ");
+        scanf("%d", &s.roll);
+
+        printf("Marks: ");
+        scanf("%f", &s.marks);
+
+        
+        fprintf(fp, "%s %d %.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fp);
+
+    
+    fp = fopen("students.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file for reading.\n");
+        return 1;
+    }
+
+    printf("\n--- Student Records from File ---\n");
+    while (fscanf(fp, "%s %d %f", s.name, &s.roll, &s.marks) == 3) {
+        printf("Name: %s, Roll: %d, Marks: %.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
